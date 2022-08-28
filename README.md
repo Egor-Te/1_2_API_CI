847754ed48ad3c66c9de585821692543fad304f2
Egor-Te <103870358+Egor-Te@users.noreply.github.com>
2022-08-28 20:34:56 +0800

    All Test Passed

---
+++ README.md
@@ -0,0 +1,4 @@
+# aqa-code
+[![Build status](https://ci.appveyor.com/api/projects/status/st2jlk2qfjqokpfh?svg=true)](https://ci.appveyor.com/project/Egor-Te/1-2-api-ci)
+
+
---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,69 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200);
+    }
+
+
+    @Test
+    void shouldContetType() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("demo/accounts")
+                .then()
+                //.header("Content-Type", "application/json; charset=UTF-8")
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    void shouldBody3() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("demo/accounts")
+                .then()
+                .body("", hasSize(3));
+    }
+
+    @Test
+    void shouldCurrencyUSD() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("demo/accounts")
+                .then()
+                .body("[1].currency", equalTo("USD"));
+    }
+
+    @Test
+    void shouldBalancePositive() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("demo/accounts")
+                .then()
+                .body("[2].balance", greaterThanOrEqualTo(0));
+    }
+
+
+}
+
+
